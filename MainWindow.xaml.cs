f248196e0e91f8086e435529bef1ad8758f7cec7
Alienware <moycarlos09@gmail.com>
2024-07-10 18:54:20 -0400

    falta

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,8 +17,27 @@ namespace CoinViewUI;
 /// </summary>
 public partial class MainWindow : Window
 {
+    public ObservableCollection<CryptoCurrency> Cryptocurrencies { get; set; }
+
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = this;
+
+        // Inicializar la colección de criptomonedas (esto es un ejemplo, debes llenarla con datos reales)
+        Cryptocurrencies = new ObservableCollection<CryptoCurrency>
+        {
+            new CryptoCurrency { Name = "Bitcoin", Price = "$4000", Change = "0.05 %", MarketCap = "750M" },
+            new CryptoCurrency { Name = "Ethereum", Price = "$2500", Change = "-0.02 %", MarketCap = "300M" },
+            new CryptoCurrency { Name = "Bitcoin", Price = "$4000", Change = "0.05 %", MarketCap = "750M" }
+        };
     }
+    public class CryptoCurrency
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Change { get; set; }
+        public string MarketCap { get; set; }
+    }
+
 }
---
+++ Usercontrol/Card.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CoinViewUI.Usercontrol;
+
+public partial class Card : UserControl
+{
+    public Card()
+    {
+        InitializeComponent();
+    }
+}
